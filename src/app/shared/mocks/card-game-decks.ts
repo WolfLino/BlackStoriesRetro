c577b49845f326d9990d55cb0e1165d77b845e90
Ulisses Lino <ulissese.lino@outlook.com>
2022-09-05 08:56:44 -0300

    Atualização dos textos das cartas

--- src/app/shared/mocks/card-game-decks.ts
+++ src/app/shared/mocks/card-game-decks.ts
@@ -6,14 +6,14 @@ export class CardGameDecks {
       id: 1,
       title: 'Crimes em Los Angeles',
       description:
-        'Em meado dos anos 90, os <b>policiais de Los Angeles</b> começam a registrar um <b>aumento na criminalidade ao oeste do continente africano<b>.',
+        'Em meado dos anos 90, boa parte dos crimes de <b>Los Angeles</b> não ocorreu na cidade.',
       imagePath: 'assets/cards/Card1-front.png',
       altImageText: 'Imagem do mapa mundi',
       backImagePath: 'assets/cards/Card1-back.png',
       backText1:
         'A polícia de Los Angeles registrava <b>casos que não tinham sua localização definida</b> no próprio endereço da delegacia.',
       backText2:
-        "Porém quando geravam relatórios, o bairro da delegacia tinha uma <b>quantidade desproporcional de delitos</b>. Por esse motivo, acharam melhor registrar tais casos nas <b>coordenadas 0, 0, 0</b>.",
+        "Ao gerar relatórios, o bairro da delegacia tinha uma <b>quantidade desproporcional de delitos</b>. Com isso, acharam melhor registrar tais casos nas <b>coordenadas 0, 0, 0</b> que fica no Oceano Atlântico ao oeste do continente africano.",
     },
     {
       id: 2,
@@ -24,19 +24,19 @@ export class CardGameDecks {
       altImageText:
         'Imagem de uma mulher encostada em um balcão de um bar, com uma pessoa a observando do outro lado do balcão.',
       backImagePath: 'assets/cards/Card2-back.png',
-      backText1: 'Após alguns dias do funcionário <b>John Null</b> registrar-se no sistema e ver que seu usuário havia sumido no dia seguinte,',
-      backText2: 'foi descoberto que devido ao seu sobrenome ser <b>Null</b> (<i>o que comumente indica nulo para computadores</i>) e ao sistema fazer limpezas de registros, seu usuário era apagado por engano a cada limpeza.'
+      backText1: 'Após alguns dias do funcionário <b>Steven Null</b> registrar-se no sistema e ver que seu usuário havia sumido no dia seguinte,',
+      backText2: 'foi descoberto que devido ao seu sobrenome ser <b>Null</b> (<i>o que comumente indica nulo para computadores</i>), ao sistema fazer as limpezas de registros, o seu usuário era marcado como lixo eletrônico.'
     },
     {
       id: 3,
-      title: 'Londres 90 graus',
+      title: 'Verão de 90 graus',
       description:
-        '<b>Em um verão na cidade de Londres</b> a temperatura em certos locais chegam a atingir 90 graus.',
+        '<b>Em um verão em uma cidade</b> a temperatura em certos locais chegam a atingir 90 graus.',
       imagePath: 'assets/cards/Card3-front.png',
       altImageText:
         'Desenho da Elizabeth\' Tower.',
       backImagePath: 'assets/cards/Card3-back.jfif',
-      backText2: 'Devido a uma infeliz decisão na construção de um edifício na cidade de Londres, foram colocados diversas janelas reflexivas em um edifícil de curvo (quase concavo). Como resultado, a luz solar era refletida em um ponto específico, que chegava a atingir 90 graus e até danificar alguns carros.'
+      backText2: 'Um efifício foi contruído na cidade de Londres em 2013, porém por causa de sua forma curva (meio côncavo), material altamente reflexivo nas janelas e sua altura fez com que os raios solares fossem concentrados em alguns blocos da cidade, o que resultou em alguns carros derretidos.'
     },
   ];
 }
